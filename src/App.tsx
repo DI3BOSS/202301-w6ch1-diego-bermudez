bceb31a5d9b8c42aad8fbb36a20681e1a8978fdd
DI3BOSS <diegofbermudez@gmail.com>
2023-02-14 23:39:32 +0100

    Modify ToDos tasks array

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import List from "./components/List/List";
 
 const App = (): JSX.Element => {
   return (
     <>
       <Header />
+      <List />
     </>
   );
 };
